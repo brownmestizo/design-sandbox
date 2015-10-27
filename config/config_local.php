542c7d97917304f3aa66e9bbf563cba683c595b3
Raden Sucalit <brownmestizo@gmail.com>
2015-10-27 19:23:25 +1000

    Updated local config file

--- config/config_local.php
+++ config/config_local.php
@@ -7,7 +7,7 @@ $manager->setConfiguration(array (
   'classname' => 'Propel\\Runtime\\Connection\\ConnectionWrapper',
   'dsn' => 'mysql:host=localhost;dbname=rathmell_mpm',
   'user' => 'root',
-  'password' => '',
+  'password' => 'pjTDJyLtga',
   'attributes' =>
   array (
     'ATTR_EMULATE_PREPARES' => false,

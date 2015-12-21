e162d6379e3e23ff98d2e85d78b4bc086a4bafd5
Raden Sucalit <brownmestizo@gmail.com>
2015-12-21 17:27:55 +1000

    Updated products search page

--- mpm-central/products.php
+++ mpm-central/products.php
@@ -4,14 +4,23 @@ require_once '../lib/init.php';
 $loader = new Twig_Loader_Filesystem('views/');
 $twig = new Twig_Environment($loader);
 
+// Variables
+$pageNumber = isset($_GET['page']) ? $_GET['page'] : 1;
+
 $categories = TblMenusQuery::create()
-	->orderByMenuName('asc')
+	->orderByMenuName('ASC')
 	->find();
+
+$productsPager = TblProdInfoQuery::create()
+	->orderByProdId('DESC')	
+	->paginate($page = $pageNumber, $maxPerPage = $maxResultsPerPage);
 
 echo $twig->render(
 	'pages_product.html', 
 		array(
 			'categories' => $categories,
+            'productsPager' => $productsPager,			
+			'pageNumber' => $pageNumber,
 		));
 
 ?>

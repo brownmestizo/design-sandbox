17883fd8c8f344177a33e1f8b4f6e6bc50244512
Raden Sucalit <brownmestizo@gmail.com>
2015-12-22 21:05:50 +1000

    Updated dashboard, categories page, and products list page

---
+++ mpm-central/categories.php
@@ -0,0 +1,67 @@
+<?php
+require_once '../lib/init.php';
+
+$loader = new Twig_Loader_Filesystem('views/');
+$twig = new Twig_Environment($loader);
+
+
+$eras = TblEraQuery::create()
+	->orderByEraId('ASC')
+	->limit(5)
+	->find();
+
+$pricingCategories = TblProdPricingQuery::create()
+	->orderByProdPriceId('ASC')
+	->limit(5)
+	->find();
+
+$shippingCategories = TblProdShippingQuery::create()
+	->orderByProdShippingName('ASC')
+	->limit(5)
+	->find();	
+
+$erasListing = [];
+$pricingCategoriesListing = [];
+$shippingCategoriesListing = [];
+
+foreach ($eras as $era) {	
+	$count = TblProdInfoQuery::create()
+	->filterByProdEra($era->getEraId())
+	->count();
+
+	$erasListing[] = [
+		"era" => $era->getEraDescription(),
+		"qty" => $count,
+	];
+}
+
+foreach ($pricingCategories as $pricingCategory) {	
+	$count = TblProdInfoQuery::create()
+	->filterByProdPriceId($pricingCategory->getProdPriceId())
+	->count();
+
+	$pricingCategoriesListing[] = [
+		"pricingCategory" => $pricingCategory->getProdPriceName(),
+		"qty" => $count,
+	];
+}
+
+foreach ($shippingCategories as $shippingCategory) {	
+	$count = TblProdInfoQuery::create()
+	->filterByProdCategoryShipping($shippingCategory->getProdShippingPriceId())
+	->count();
+
+	$shippingCategoriesListing[] = [
+		"shippingCategory" => $shippingCategory->getProdShippingName(),
+		"qty" => $count,
+	];
+}
+
+echo $twig->render(
+	'pages_categories.html', 
+		array(
+			'eras' => $erasListing,
+			'pricingCategories' => $pricingCategoriesListing,
+			'shippingCategories' => $shippingCategoriesListing,
+		));
+?>

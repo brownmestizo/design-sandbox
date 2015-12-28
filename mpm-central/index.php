49d791dc60d3c0e13f606f3d30f84974d5ba3388
Raden Sucalit <brownmestizo@gmail.com>
2015-12-28 14:39:37 +1100

    Updated dashboard variables

--- mpm-central/index.php
+++ mpm-central/index.php
@@ -4,24 +4,32 @@ require_once '../lib/init.php';
 $loader = new Twig_Loader_Filesystem('views/');
 $twig = new Twig_Environment($loader);
 
-$countMb = TblProdInfoQuery::create()
-	->filterByMb(1)
-	->count();
+$count = TblProdInfoQuery::create()->count();
+$countMb = TblProdInfoQuery::create()->filterByMb(1)->count();
+$countPa = TblProdInfoQuery::create()->filterByPa(1)->count();	
+$countMt = TblProdInfoQuery::create()->filterByM3(1)->count();	
+$countPa = TblProdInfoQuery::create()->filterByPa(1)->count();	
+$smallerModels = TblProdInfoQuery::create()->filterByProdSaveas(1)->count();
+$stands = TblStandsQuery::create()->count();
 
-$countPa = TblProdInfoQuery::create()
-	->filterByPa(1)
-	->count();	
-
-$countMt = TblProdInfoQuery::create()
-	->filterByM3(1)
-	->count();	
+$eras = TblEraQuery::create()->count();
+$pricingCategories = TblProdPricingQuery::create()->count();
+$shippingCategories = TblProdShippingQuery::create()->count();	
+$categories = TblMenusQuery::create()->count();
 
 echo $twig->render(
 	'pages_index.html', 
 		array(
 			'countMb' => $countMb,				
 			'countPa' => $countPa,			
-			'countMt' => $countMt,			
+			'countMt' => $countMt,	
+			'count' => $count,		
+			'eras' => $eras,
+			'pricingCategories' => $pricingCategories,
+			'shippingCategories' => $shippingCategories,
+			'categories' => $categories,
+			'smallerModels' => $smallerModels,
+			'stands' => $stands,
 		));
 
 ?>

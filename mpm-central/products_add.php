ee5bd139d116a7e4c54bc393a238991cdc574101
Raden Sucalit <brownmestizo@gmail.com>
2016-03-17 18:37:13 +1000

    Updated product add form

--- mpm-central/products_add.php
+++ mpm-central/products_add.php
@@ -1,12 +1,30 @@
 <?php
 require_once '../lib/init.php';
 
+/*
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+*/
+
 $loader = new Twig_Loader_Filesystem('views/');
 $twig = new Twig_Environment($loader);
 
+/*
+$formFactory = Forms::createFormFactory();
+
+$form = $formFactory->createBuilder()
+->add('task', TextType::class)
+->add('dueDate', DateType::class)
+->getForm();	
+
+$twig->render('sample_form.html', array(
+    'form' => $form->createView(),
+));
+*/
+
 echo $twig->render(
 	'pages_product_add.html', 
-		array(
+		array());
 
-		));
 ?>

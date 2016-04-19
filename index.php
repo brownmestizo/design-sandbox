e5a8952456cfeba68646544c2a989f2d16c5bb32
Raden Sucalit <brownmestizo@gmail.com>
2016-04-19 18:46:34 +1000

    Added index to root

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>MPM Landing page</title>    
+    <!-- Bootstrap core CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="mpm-central/assets/css/style.css" rel="stylesheet">
+    <!-- Just for debugging purposes. -->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- include pace script for automatic web page progress bar  -->
+
+    <script>
+        paceOptions = {
+            elements: true
+        };
+    </script>    
+</head>
+
+<body id="request">
+
+    <div id="wrapper">
+
+        <nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav metismenu" id="side-menu">
+                    <li class="nav-header">
+                        <div class="logo">MPM<span class="meta-normal">Central</span>
+                        </div>
+                        <div class="logo-element">MPM</div>
+                    </li>
+                </ul>
+            </div>
+        </nav><!-- END nav -->                
+
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+
+            <div class="row">
+                <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+                    <div class="navbar-header">
+                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+                    </div>        
+                    
+                    <div class="navbar-search">
+                    </div>            
+                </nav>
+            </div>
+
+            <div class="jumbotron">
+              <h1>Hello, world!</h1>
+              <p>Sorry, there's nothing here. You have been misled.</p>
+              <p><a class="btn btn-primary btn-lg" href="modelbuffs.com/" role="button">Looking for Modelbuffs?</a></p>
+            </div>            
+       
+        </div> <!-- END page wrapper -->
+
+    </div> <!-- END wrapper -->
+
+    <!-- Mainly scripts -->
+    <script src="mpm-central/assets/js/jquery-2.1.1.js"></script>
+    <script src="mpm-central/assets/js/bootstrap.min.js"></script>
+    <script src="mpm-central/assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="mpm-central/assets/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="mpm-central/assets/js/inspinia.js"></script>
+    <script src="mpm-central/assets/js/plugins/pace/pace.min.js"></script>
+</body>
+</html>

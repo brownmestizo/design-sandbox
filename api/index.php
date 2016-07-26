e7556d768a08f58b90cd4244cb6dafa0d34a0803
Raden Sucalit <brownmestizo@gmail.com>
2016-07-26 09:34:03 +0000

    Updated category pages to use the proper add to cart button

--- api/index.php
+++ api/index.php
@@ -1,13 +1,14 @@
 <?php 
-
+	
+	
 	function is_localhost() {
-	    $whitelist = array( '127.0.0.1', '::1' );
+	    $whitelist = array( '127.0.0.1', '::1', '192.168.33.1',);
 	    if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist) )
 	        return true;
 	}
-
-
+	
 	require_once '../vendor/autoload.php';	
+
 
 	if(is_localhost()) require_once '../config/config_local.php';
 	else require_once '../config/config_online.php';	
